b08c55e93826a868478cb9faeaf805424648c931
Harold_Mesa <harold.mesa@icloud.com>
2022-12-20 23:08:42 -0600

    Successfully added route protection to server. WIP: Adding Auth tokens to request headers in front-end.

--- server.js
+++ server.js
@@ -8,16 +8,7 @@ import { MongoClient } from 'mongodb';
 //import dotenv in order to use environmental variables
 import {} from 'dotenv/config';
 import { db, connectToDb } from './db.js';
-import { send } from 'process';
-// import Twitter from 'twitter';
 
-//create a new instance of the Twitter client
-// const client = new Twitter({
-//   consumer_key: process.env.TWITTER_CONSUMER_KEY,
-//   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-//   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-//   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-// });
 
 // Setting firebase admin package before initializing express app
 const credentials = JSON.parse(fs.readFileSync('./firebaseCredentials.json'));
@@ -30,19 +21,23 @@ const app = express();
 //express.json() is a built in middleware function in Express. It parses incoming JSON requests and puts the parsed data in req.body.
 app.use(express.json());
 
-//This function loads automatically the user's credentials into req.user
+//Adding middleware to these routes to check if the user is logged in
 app.use(async (req, res, next) => {
 //set headers to allow requests from client
-  const { authToken } = req.headers;
-  if(authToken){
+  const { authtoken } = req.headers;
+
+  if(authtoken){
     try {
       //verify the token sent from the client, and loads the user's credentials into req.user
-        const user = await admin.auth().verifyIdToken(authToken);
-        req.user = user;
+      req.user = await admin.auth().verifyIdToken(authtoken);
     } catch (error) {
-      res.sendStatus(400);
+      //if the token is invalid, send a 400 status code to the client
+      return res.sendStatus(400);
     }
   }
+  //This is a workaround to avoid the error "TypeError: Cannot set property 'user' of undefined"
+  req.user = req.user || {};
+
   next();
 })
 
@@ -50,6 +45,7 @@ app.use(async (req, res, next) => {
 app.get('/api/articles/:name/', async (req, res) => {
   //get the value of :name and use it to req (query) to MongoDB
   const { name } = req.params;
+  const { uid } = req.user;
   //connect to MongoDB and make a query
   const uri = `mongodb+srv://${process.env.MONGO_URI}@cluster0.c9syyx6.mongodb.net/?retryWrites=true&w=majority`;
   const client = new MongoClient(uri);
@@ -61,43 +57,13 @@ app.get('/api/articles/:name/', async (req, res) => {
   const article = await db.collection('articles').findOne({name});
   //if article was found in the query, send response (as JSON data, to make sure the correct headers are set on the response) to client; else, display error message
   if(article){
+  // Adding extra property to articles document to store the user ids of the users that have upVoted the article
+    const upvoteIds = article.upvoteIds || [];
+    article.canUpVote = uid && !upvoteIds.includes(uid);
     res.json(article);
   } else {
     res.sendStatus(404);
   }
-});
-
-//UPDATE article data (upVotes prop)
-app.put('/api/articles/:name/upVotes', async (req,res) => {
-  const { name } = req.params;
-  //use MongoDB $inc operator to increment by one the upVotes prop for the specific article
-  await db.collection('articles').updateOne({ name }, { $inc: {upVotes: 1}} );
-  //query (READ) data from specific article by name
-  const article = await db.collection('articles').findOne({name});
-  //response with updated data
-  if(article){
-    res.json(article);
-  } else {
-    res.send('This article doesn\'t exists')
-  }
-});
-
-//CREATE(add) comments to database 
-app.post('/api/articles/:name/comments', async (req, res) => {
-  const { name } = req.params;
-  const { postedBy, text } = req.body;
-
-  await db.collection('articles').updateOne({ name }, {
-    $push: { comments: { postedBy, text }}
-  })
-  const article = await db.collection('articles').findOne({name})
-
-  //response with updated data
-  if(article){
-    res.json(article);
-  } else {
-    res.send('This article doesn\'t exists')
-  }  
 });
 
 //READ count of articles in database
@@ -112,8 +78,12 @@ app.get('/api/articles/', async (req, res) => {
 
 //READ only first 9 articles from database
 app.get('/api/articles-list/', async (req, res) => {
+  const { uid } = req.user;
   const articles = await db.collection('articles').find({}).limit(9).toArray();
+
   if(articles){
+  const upvoteIds = articles.upvoteIds || [];
+  articles.canUpVote = uid && !upvoteIds.includes(uid);
   res.json(articles);
   } else {
     res.send('Error');
@@ -123,13 +93,77 @@ app.get('/api/articles-list/', async (req, res) => {
 //READ only next 9 articles from database at a time starting from the 10th article, and keep that pattern
 app.get('/api/articles-list/:count', async (req, res) => {
   const { count } = req.params;
+  const { uid } = req.user;
+
   const articles = await db.collection('articles').find({}).skip(parseInt(count)).limit(9).toArray();
+
   if(articles){
+  const upvoteIds = articles.upvoteIds || [];
+  articles.canUpVote = uid && !upvoteIds.includes(uid);
   res.json(articles);
   } else {
     res.send('Error');
   }
 });
+
+//Adding middleware that will only apply to these two routes. If the user is not logged in, it will send a 401 status code to the client
+app.use((req, res, next) => {
+  if(req.user){
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+})
+
+//UPDATE article data (upVotes prop)
+app.put("/api/articles/:name/upVotes", async (req, res) => {
+  const { name } = req.params;
+  const { uid } = req.user;
+
+  const article = await db.collection("articles").findOne({ name });
+
+  if (article) {
+    const upvoteIds = article.upvoteIds || [];
+    const canUpVote = uid && !upvoteIds.includes(uid);
+
+    if (canUpVote) {
+      //use MongoDB $inc operator to increment by one the upVotes prop for the specific article
+      await db
+        .collection("articles")
+        .updateOne(
+          { name },
+          { $inc: { upVotes: 1 }, $push: { upvoteIds: uid } }
+        );
+    }
+
+    //query (READ) data from specific article by name
+    const updatedArticle = await db.collection("articles").findOne({ name });
+    //response with updated data
+    res.json(updatedArticle);
+  } else {
+    res.send("This article doesn't exists");
+  }
+});
+
+//CREATE(add) comments to database 
+app.post('/api/articles/:name/comments', async (req, res) => {
+  const { name } = req.params;
+  const { text } = req.body;
+  const { email } = req.user;
+
+  await db.collection('articles').updateOne({ name }, {
+    $push: { comments: { postedBy: email, text }}
+  })
+  const article = await db.collection('articles').findOne({name})
+
+  //response with updated data
+  if(article){
+    res.json(article);
+  } else {
+    res.send('This article doesn\'t exists')
+  }  
+});
+
 
 //telling the server to listen on port 8000 and pass a callback to display message to check it is working
 connectToDb(() =>{
